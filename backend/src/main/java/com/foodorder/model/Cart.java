42a3c9edbec653bc1a63f1adbac99b2aba444fab
ShivaShankar1233 <shivashankarw0123@gmail.com>
2026-03-31 11:41:08 +0530

    Create Cart entity model class

---
+++ backend/src/main/java/com/foodorder/model/Cart.java
@@ -0,0 +1,33 @@
+package com.foodorder.model;
+
+public class Cart {
+    private Long id;
+    private Long userId;
+    private List<Long> itemIds;
+
+    public Cart() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public List<Long> getItemIds() {
+        return itemIds;
+    }
+
+    public void setItemIds(List<Long> itemIds) {
+        this.itemIds = itemIds;
+    }
+}
