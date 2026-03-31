f1ca39635ad5eb18f20c0c7138886d6f012f657b
ShivaShankar1233 <shivashankarw0123@gmail.com>
2026-03-31 11:35:33 +0530

    Create FoodOrderingApplication main class

---
+++ backend/src/main/java/com/foodorder/FoodOrderingApplication.java
@@ -0,0 +1,7 @@
+package com.foodorder;
+
+public class FoodOrderingApplication {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Food Ordering Application!");
+    }
+}
