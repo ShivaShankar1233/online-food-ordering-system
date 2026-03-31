a90d8dba5b7ddcb2793710e3cb9afb092cdad727
ShivaShankar1233 <shivashankarw0123@gmail.com>
2026-03-31 11:35:38 +0530

    Create GlobalExceptionHandler class

---
+++ backend/src/main/java/com/foodorder/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.foodorder.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleException(Exception ex) {
+        // You can log the exception here
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // Add more exception handlers as needed
+}
