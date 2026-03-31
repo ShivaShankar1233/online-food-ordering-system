08fb95430bcdde76504e5c9e34ec9ca69a7218c1
ShivaShankar1233 <shivashankarw0123@gmail.com>
2026-03-31 11:36:22 +0530

    Create EmailService class

---
+++ backend/src/main/java/com/foodorder/service/EmailService.java
@@ -0,0 +1,7 @@
+package com.foodorder.service;
+
+public class EmailService {
+    public void sendEmail(String recipient, String subject, String body) {
+        // Logic to send email goes here.
+    }
+}
