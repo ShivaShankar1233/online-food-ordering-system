ad9750c86b0e6d0b5df3f0263daec61d7c9020d5
ShivaShankar1233 <shivashankarw0123@gmail.com>
2026-03-31 11:38:16 +0530

    Create RestaurantService class

---
+++ backend/src/main/java/com/foodorder/service/RestaurantService.java
@@ -0,0 +1,21 @@
+package com.foodorder.service;
+
+public class RestaurantService {
+    // Implementation of RestaurantService
+    
+    public void addRestaurant(String name, String location) {
+        // Code to add a restaurant
+    }
+
+    public void removeRestaurant(String name) {
+        // Code to remove a restaurant
+    }
+
+    public void updateRestaurant(String name, String newLocation) {
+        // Code to update restaurant details
+    }
+
+    public void getRestaurants() {
+        // Code to get a list of all restaurants
+    }
+}
