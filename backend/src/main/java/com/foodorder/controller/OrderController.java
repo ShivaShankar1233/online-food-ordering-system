09783ee433a323504b8e9106f64c91342f748dc3
ShivaShankar1233 <shivashankarw0123@gmail.com>
2026-03-31 11:35:29 +0530

    Create OrderController class

---
+++ backend/src/main/java/com/foodorder/controller/OrderController.java
@@ -0,0 +1,9 @@
+package com.foodorder.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    // Add your methods here
+}
